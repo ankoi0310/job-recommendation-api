4ae0b710ab847bf1be74684351f74a0e1ade9c81
Huynh Van Huu An <huynhvahuuan3620@gmail.com>
2022-12-24 19:07:06 +0700

    initial

---
+++ src/test/java/it/koi/JobRec/JobRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.koi.JobRec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
